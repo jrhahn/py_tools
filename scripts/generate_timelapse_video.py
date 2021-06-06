a5bb753ef200b7dff3c607a6a788f41a29ac8635
Jürgen Hahn <juergen_hahn@gmx.net>
2021-06-06 20:30:51 +0200

    face detection

--- scripts/generate_timelapse_video.py
+++ scripts/generate_timelapse_video.py
@@ -1,6 +1,8 @@
-import pandas as pd
+from typing import Tuple
+
 import argparse
 
+import numpy as np
 from PIL import Image, ExifTags
 from pathlib import Path
 from os import makedirs, system, remove
@@ -9,38 +11,126 @@ import logging
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+import cv2
+
+# Load the cascade
+# source: https://github.com/opencv/opencv/tree/master/data/haarcascades
+path_cascade = Path('.').resolve().parents[0] / 'resources' / 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(str(path_cascade))
 
 
-def rotate_images(
-        path_source: Path,
-        path_destination: Path
-) -> Path:
-    path_tmp = path_destination / 'tmp'
+def detect_face(
+        img: Image
+) -> Tuple[float, float]:
+    open_cv_image = np.array(img.convert('RGB'))
+    # Convert RGB to BGR
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+
+    gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    if len(faces) > 0:
+        left = faces[0][0]
+        top = faces[0][1]
+        return left, top
+
+    return None, None
+
+
+def scale_to_target_size(
+        img: Image,
+        target_width: float,
+        target_height: float
+) -> Image:
+    img_size = img.size
+    aspect = img_size[1] / img_size[0]
+
+    aspect_tgt = target_height / target_width
+
+    if aspect > aspect_tgt:
+        # align on width and crop height
+        img = img.resize((target_width, int(aspect * target_width)))
+    else:
+        # align on height and crop on width
+        img = img.resize((int(target_height / aspect), target_height))
+
+    face_left, face_top = detect_face(img=img)
+
+    diff_x = img.size[0] - target_width
+    diff_y = img.size[1] - target_height
+
+    weight_x = 0.5
+    weight_y = 0.5
+    if face_left is not None:
+        weight_x = face_left / img.size[0]
+        weight_y = face_top / img.size[1]
+
+    if diff_x < 0:
+        raise ValueError("diffX is negative")
+
+    if diff_y < 0:
+        raise ValueError("diffY is negative")
+
+    img_crop = img.crop(
+        (
+            int(diff_x * weight_x),
+            int(diff_y * weight_y),
+            int(diff_x * weight_x) + target_width,
+            int(diff_y * weight_y) + target_height
+        )
+    )
+
+    return img_crop
+
+
+def fix_rotation(img: Image) -> Image:
+    exif = img._getexif()
 
     for orientation in ExifTags.TAGS.keys():
         if ExifTags.TAGS[orientation] == 'Orientation':
             break
 
-    for ff in path_source.rglob('*'):
-        if not ff.is_file():
-            continue
+    if exif[orientation] == 3:
+        img = img.rotate(180, expand=True)
+    elif exif[orientation] == 6:
+        img = img.rotate(270, expand=True)
+    elif exif[orientation] == 8:
+        img = img.rotate(90, expand=True)
 
-        file_out_ = Path(str(ff).replace(str(path_source), str(path_tmp)))
+    return img
 
-        makedirs(file_out_.parents[0], exist_ok=True)
 
-        img = Image.open(ff)
+# todo make image size constant
+def preprocess_images(
+        path_source: Path,
+        path_destination: Path,
+        target_width: int = 1080,
+        target_height: int = 1920
+) -> Path:
+    path_tmp = path_destination / 'tmp'
 
-        exif = img._getexif()
-
-        if exif[orientation] == 3:
-            img = img.rotate(180, expand=True)
-        elif exif[orientation] == 6:
-            img = img.rotate(270, expand=True)
-        elif exif[orientation] == 8:
-            img = img.rotate(90, expand=True)
-
-        img.save(file_out_)
+    # for ff in path_source.rglob('*'):
+    #     if not ff.is_file():
+    #         continue
+    #
+    #     logger.info(f"Preparing {ff} ..")
+    #
+    #     file_out_ = Path(str(ff).replace(str(path_source), str(path_tmp)))
+    #
+    #     makedirs(file_out_.parents[0], exist_ok=True)
+    #
+    #     img = Image.open(ff)
+    #
+    #     img = fix_rotation(img)
+    #     img = scale_to_target_size(
+    #         img=img,
+    #         target_width=target_width,
+    #         target_height=target_height
+    #     )
+    #     logger.info(f"  -> {img.size}")
+    #
+    #     img.save(file_out_)
 
     return path_tmp
 
@@ -51,7 +141,7 @@ def generate_file_list(
         filename: str = 'filelist.txt'
 ) -> Path:
     path_file = path_destination / filename
-    files = sorted(list(f'file {ff}\n' for ff in path_source.rglob('*') if ff.is_file()))
+    files = sorted(list(f"file '{ff}' \n" for ff in path_source.rglob('*') if ff.is_file()))
 
     with open(path_file, 'w') as f:
         f.writelines(files)
@@ -62,12 +152,14 @@ def generate_file_list(
 def run(
         path_source: Path,
         path_destination: Path,
-        fps: int = 10
+        target_width: int = 1080,
+        target_height: int = 1920,
+        fps: int = 1
 ):
     makedirs(path_destination, exist_ok=True)
     logger.info(f"Reading from {path_source} ..")
 
-    path_tmp = rotate_images(
+    path_tmp = preprocess_images(
         path_source=path_source,
         path_destination=path_destination
     )
@@ -77,11 +169,11 @@ def run(
         path_destination=path_destination
     )
 
-    cmd = f'ffmpeg -y -f concat -safe 0 -i {path_file_list} -c:v libx264 -vf "fps=fps={fps},format=yuv420p" {path_destination / "output.mp4"}'
+    cmd = f'ffmpeg -y -r {1/fps} -f concat -safe 0 -i {path_file_list} -s {target_width}x{target_height}  -c:v libx264 -vf "fps=fps={fps}" {path_destination / "output.mp4"}'
 
     system(cmd)
 
-    remove(path_file_list)
+    # remove(path_file_list)
 
 
 if __name__ == '__main__':
